955ae9f1b7562c22f5db40416f93cd8262852328
jiyongseong <jiyongseong@hotmail.com>
2016-10-27 08:14:46 +0900

    aws elastic ip

---
+++ azure_and_aws/network/elastic_ip_reserved_ip/aws_elastic_ip.ps1
@@ -0,0 +1,18 @@
+﻿#http://docs.aws.amazon.com/ko_kr/AWSEC2/latest/UserGuide/elastic-ip-addresses-eip.html
+
+Get-AWSCredentials -ListProfiles
+
+$profileName = "default"
+$region = "ap-southeast-2"
+
+Initialize-AWSDefaults -ProfileName $profileName -Region $region
+
+New-EC2Address -Domain standard
+
+Get-EC2Address
+
+$allocationId = "eipalloc-########"
+
+Remove-EC2Address -AllocationId $allocationId -Force
+
+
