2c23e2f2680459e27aa8ff8d1b4276b1bbbca170
jiyongseong <jiyongseong@hotmail.com>
2017-04-04 11:42:25 +0900

    UoA

--- temp/UoA/SQLQuery1.sql
+++ temp/UoA/SQLQuery1.sql
@@ -1,4 +1,4 @@
-
+﻿
 --drop table tb_temp_course
 
 create table tb_temp_course
@@ -12,11 +12,19 @@ create table tb_temp_course
 )
 go
 
-select * from  tb_temp_course 
+select * from  tb_temp_course a  where prerequisite > ''
 
-select courseA, prerequisite
-from tb_temp_course 
-where courseA = 'PHYSICS 203'
+select  prerequisite from tb_temp_course a where courseA = 'COMPSYS 726' and a.
 --truncate table tb_temp_course
  
  
+
+ Prerequisite: At least one of PHYSICS 220-261 
+ Prerequisite: 15 points from PHYSICS 120, 160 and 15 points from MATHS 108, 110, 150 
+ Prerequisite: B– average in one of ENGSCI 211, MATHS 253, PHYSICS 211 and either PHYSICS 201 or 231 
+ Prerequisite: B– in PHYSICS 202 or 261, and 15 points from ENGSCI 211, MATHS 253, PHYSICS 211 
+ Prerequisite: PHYSICS 701, or MATHS 340 and 361 
+ Restriction: CHEMMAT 774, 775, 776 
+ To complete this course students must enrol in CHEMMAT 788 A and B 
+ Prerequisite: 45 points at Stage II Psychology and 15 points from STATS 101-125, 191 
+ Prerequisite: ENGGEN 140 or CHEM 110 or 120 
