533d69033eb98cecdac83e3adfbb1656474f70e4
jiyongseong <jiyongseong@hotmail.com>
2017-05-31 11:41:29 +0900

    unzip

---
+++ powershell/fun/unzip/unzip.ps1
@@ -0,0 +1,19 @@
+﻿function Expand-ZIPFile($file, $destination)
+{
+    $shell = new-object -com shell.application
+    $zip = $shell.NameSpace($file)
+    foreach($item in $zip.items())
+    {
+        $shell.Namespace($destination).copyhere($item)
+    }
+}
+
+$src = '<<원본 파일 경로>>'
+$dest = '<<대상 경로>>'
+
+
+$files = Get-ChildItem -Path $src -Filter *.zip
+
+foreach ($file in $files){
+    Expand-ZIPFile –File $file.FullName –Destination $dest
+}
