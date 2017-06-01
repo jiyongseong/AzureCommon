c69f1159d57f9a97b0956bf83e4be564870aa892
jiyongseong <jiyongseong@hotmail.com>
2017-06-01 16:17:31 +0900

    setting java environment

---
+++ powershell/fun/setting_java_environment_variables/scripts.ps1
@@ -0,0 +1,8 @@
+﻿## run as administrator
+[Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\Java\jdk1.8.0_131", "Machine")
+$env:Path += "%JAVA_HOME%\bin"
+
+
+[Environment]::GetEnvironmentVariable("JAVA_HOME", "Machine")
+$path = Get-Item Env:Path | Select value
+$path.Value.Split(";")
