6f6197922997c2bc5ec5470bce8249ecf544eab2
jiyongseong <jiyongseong@hotmail.com>
2017-06-01 16:18:44 +0900

    update

--- powershell/fun/setting_java_environment_variables/scripts.ps1
+++ powershell/fun/setting_java_environment_variables/scripts.ps1
@@ -2,7 +2,7 @@
 [Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\Java\jdk1.8.0_131", "Machine")
 $env:Path += "%JAVA_HOME%\bin"
 
-
+## check
 [Environment]::GetEnvironmentVariable("JAVA_HOME", "Machine")
 $path = Get-Item Env:Path | Select value
 $path.Value.Split(";")
