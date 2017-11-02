617c6b8ff31deba639f6bc34e7d2b41791d2d7d8
Ji Yong Seong <jyseong@microsoft.com>
2017-11-02 17:06:01 +0900

    pricing

---
+++ powershell/azure/pricing/Get-VMPrice-by-myungkim.ps1
@@ -0,0 +1,68 @@
+﻿
+function createOutputObject
+{
+    $row = New-Object PSObject
+
+    $row | Add-Member -MemberType NoteProperty -Name "Product" -Value ""
+    $row | Add-Member -MemberType NoteProperty -Name "Tier" -Value ""
+    $row | Add-Member -MemberType NoteProperty -Name "Type" -Value ""
+    $row | Add-Member -MemberType NoteProperty -Name "Core" -Value ""
+    $row | Add-Member -MemberType NoteProperty -Name "RAM" -Value ""
+    $row | Add-Member -MemberType NoteProperty -Name "DiskSize" -Value ""       
+    $row | Add-Member -MemberType NoteProperty -Name "Region" -Value ""
+    $row | Add-Member -MemberType NoteProperty -Name "Price" -Value ""
+
+    $row
+}
+
+function getvms()
+{
+    Clear-Host
+    $outputFilePath = ".\VMs.csv"
+    Remove-Item $outputFilePath -ErrorAction Ignore
+
+    Write-Host "Requesting Pricing Idirnformation from https://azure.microsoft.com/api/v1/pricing/virtual-machines/calculator/" -BackgroundColor Yellow -ForegroundColor Red
+
+    $r = Invoke-WebRequest -Uri "https://azure.microsoft.com/api/v1/pricing/virtual-machines/calculator/?culture=ko-kr"
+    Write-Host "Exporting.............." -BackgroundColor Yellow -ForegroundColor Red
+
+    $offers = ($r.Content | ConvertFrom-Json) | Select -expand offers 
+
+    $vms = $offers.psobject.properties.name
+
+    $resultSet = [System.Collections.ArrayList]@()
+    $row = createOutputObject
+    
+    foreach ($vm in $vms)
+    {
+        Write-Host "Exporting $vm  ...."
+
+        $row.Core = ($offers | Select -ExpandProperty  $vm.ToString()).cores
+        $row.RAM = ($offers | Select -ExpandProperty  $vm.ToString()).ram
+        $row.DiskSize = ($offers | Select -ExpandProperty  $vm.ToString()).disksize
+        
+        $prices = ($offers | Select -ExpandProperty $vm.ToString()).prices 
+        $regions = $prices.psobject.properties.name
+       
+        foreach ($region in $regions)
+        {
+            $row.Region = $region
+            $row.Price = ($prices | Select -ExpandProperty $region.ToString())
+            $row.Product = $vm.Replace("-", " ")
+                        
+            $names = $vm.Split("-")
+            $row.Tier = $names[-2]
+            $row.Type = $names[-1]
+
+            $resultSet.Add($row.PsObject.Copy()) | Out-Null
+            
+        } 
+
+    }
+    
+    $resultSet | Export-Csv -Path $outputFilePath -NoTypeInformation 
+
+    Write-Host "Done." -BackgroundColor Yellow -ForegroundColor Red 
+}
+
+getvms 
---
+++ powershell/azure/pricing/Get-VMPrice.ps1
@@ -0,0 +1,66 @@
+﻿
+Clear-Host
+$outputFilePath = ".\VMs.csv" 
+
+Write-Host "Requesting Pricing Information from https://azure.microsoft.com/api/v1/pricing/virtual-machines/calculator/" -BackgroundColor Yellow -ForegroundColor Red
+
+$r = Invoke-WebRequest -Uri "https://azure.microsoft.com/api/v1/pricing/virtual-machines/calculator/?culture=ko-kr"
+
+
+<#
+($r.Content | ConvertFrom-Json).offers 
+($r.Content | ConvertFrom-Json).tiers
+($r.Content | ConvertFrom-Json).types
+($r.Content | ConvertFrom-Json).sizes
+
+($r.Content | ConvertFrom-Json).regions
+($r.Content | ConvertFrom-Json).resources
+($r.Content | ConvertFrom-Json).softwareLicenses
+#>
+
+Write-Host "Exporting.............." -BackgroundColor Yellow -ForegroundColor Red
+
+$offers = ($r.Content | ConvertFrom-Json) | Select -expand offers 
+
+$vms = $offers.psobject.properties.name
+
+foreach ($vm in $vms)
+{
+    Write-Host "Exporting $vm  ...."
+    $core = ($offers | Select -ExpandProperty  $vm.ToString()).cores
+    $ram = ($offers | Select -ExpandProperty  $vm.ToString()).ram
+    $disksize = ($offers | Select -ExpandProperty  $vm.ToString()).disksize
+    #$sku = ($offers | Select -ExpandProperty  $vm.ToString()).skus
+    $prices = ($offers | Select -ExpandProperty $vm.ToString()).prices 
+    
+    $regions = $prices.psobject.properties.name
+    
+    foreach ($region in $regions)
+    {
+        $price = ($prices | Select -ExpandProperty $region.ToString())
+
+        $names = $vm.Split("-")
+
+        switch ($names.Count)
+        {
+            3 { $prod = $names[0]}
+            4 { $prod = $names[0]+" "+$names[1]}
+            5 { $prod = $names[0]+" "+$names[1]+" "+$names[2]}
+        }
+
+        $row = New-Object PSObject
+
+        $row | Add-Member -MemberType NoteProperty -Name "Core" -Value $core
+        $row | Add-Member -MemberType NoteProperty -Name "RAM" -Value $ram
+        $row | Add-Member -MemberType NoteProperty -Name "DiskSize" -Value $disksize
+        $row | Add-Member -MemberType NoteProperty -Name "Region" -Value $region
+        $row | Add-Member -MemberType NoteProperty -Name "Price" -Value $price
+        $row | Add-Member -MemberType NoteProperty -Name "Product" -Value $prod
+        $row | Add-Member -MemberType NoteProperty -Name "Tier" -Value $names[-2]
+        $row | Add-Member -MemberType NoteProperty -Name "Type" -Value $names[-1]
+
+        $row| Select-Object -Property Product, Tier, Type, Core, RAM, DiskSize, Region, Price | Export-Csv -Path $outputFilePath -NoTypeInformation -Append
+    } 
+}
+
+Write-Host "Done." -BackgroundColor Yellow -ForegroundColor Red
